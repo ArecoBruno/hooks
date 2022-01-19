b092715d756829b11749b08700cff1c076cf1bc2
Bruno Areco <bruno.areco@gmail.com>
2022-01-19 11:49:48 -0300

    aula 19-01

--- src/views/examples/UseEffect.jsx
+++ src/views/examples/UseEffect.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from '../../components/layout/SectionTitle'
 
 function calcFatorial(n) {
     if(n < 0) return -1
@@ -11,6 +12,7 @@ const UseEffect = (props) => {
 
     const [num, setNum] = useState(1)
     const [fatorial, setFatorial] = useState(1)
+    const [umOutro, setumOutro] = useState("")
 
     useEffect(function() {
         setFatorial(calcFatorial(num))
@@ -22,18 +24,31 @@ const UseEffect = (props) => {
         }
     }, [fatorial])
 
+    useEffect(function() {
+        setumOutro(num % 2 === 0 ? "Par" : "Impar")
+    }, [num])
+
     return (
         <div className="UseEffect">
             <PageTitle
                 title="Hook UseEffect"
                 subtitle="Permite executar efeitos colaterais em componentes funcionais!"
             />
+            <SectionTitle title="Exercício #01" />    
             <div className="center">
                 <div>
                     <span className="text">Fatorial: </span>
                     <span className="text red">{fatorial}</span>
                 </div>
                 <input type="number" className="input" value={num} onChange={e => setNum(e.target.value)} />
+            </div>
+
+            <SectionTitle title="Exercício #02" />  
+            <div className="center">
+                <div>
+                    <span className="text">Status: </span>
+                    <span className="text red">{umOutro}</span>
+                </div>
             </div>
         </div>
     )
